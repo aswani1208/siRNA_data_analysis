db29eef2faa6d7aa580708e4c17967bb995a237b
aswani1208 krishnap <aswanikrishnap1994@gmail.com>
2025-03-22 12:22:28 +0000

    Added whole alignement step

--- read_mapping.sh
+++ read_mapping.sh
@@ -2,6 +2,7 @@
 # Install bowtie2
 
 sudo apt install bowtie2
+sudo apt install samtools
 
 # Download genome file
 
@@ -18,6 +19,7 @@ gunzip Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa.gz
 
 bowtie2-build  Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa Solanum_tuberosum.SolTub_3.0.dna.toplevel_index
 
+
 # Read mapping with reference genome
 while read SRA; do
     bowtie2 -x Solanum_tuberosum.SolTub_3.0.dna.toplevel_index \
@@ -25,4 +27,32 @@ while read SRA; do
     -S /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned.sam \
     --very-sensitive --threads 8 \
     --un /workspace/siRNA_data_analysis/read_mapping/"$SRA"_unmapped_host.fastq
+done < /workspace/siRNA_data_analysis/SRR_Acc_List.txt
+
+# view the sam file 
+
+less SRRXXXXX_host_mapped.sam
+# sam to bam file conversion
+
+while read SRA; do
+samtools view -bS /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned.sam > /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned.bam
+done < /workspace/siRNA_data_analysis/SRR_Acc_List.txt
+
+# sort bam file
+while read SRA; do
+samtools sort /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned.bam -o /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned_sorted.bam
+done < /workspace/siRNA_data_analysis/SRR_Acc_List.txt
+
+# index the refrence genome file and sorted sam file
+
+samtools faidx Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa
+
+while read SRA; do
+samtools index /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned_sorted.bam
+done < /workspace/siRNA_data_analysis/SRR_Acc_List.txt
+
+# view the alignement summary
+
+while read SRA; do
+samtools flagstats /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned_sorted.bam > /workspace/siRNA_data_analysis/read_mapping/"$SRA"_host_aligned_sorted.stats.txt
 done < /workspace/siRNA_data_analysis/SRR_Acc_List.txt
