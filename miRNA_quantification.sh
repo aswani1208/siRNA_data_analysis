630b2f48fe22ed4e20f32f2e7acbdc3f8ce11b73
aswanikrishnaP <40095406+aswani1208@users.noreply.github.com>
2025-03-23 13:14:48 +0400

    Update miRNA_quantification.sh

--- miRNA_quantification.sh
+++ miRNA_quantification.sh
@@ -31,17 +31,87 @@ wget https://www.mirbase.org/download/hairpin.fa
 wget https://www.mirbase.org/download/mature.fa
 
 # Extract miRNA
-extract_miRNAs.pl hairpin.fa tcc > hairpin_tcc.fa
-extract_miRNAs.pl mature.fa tcc > mature_tcc.fa
+
+extract_miRNAs.pl hairpin.fa stu > hairpin_stu.fa
+extract_miRNAs.pl mature.fa stu > mature_stu.fa
 
 # build bowtie index for miRNA seq from pimREN and mirbase
+
 bowtie-build Solanum_tuberosum_mature.fa Solanum_tuberosum_mature_index
 bowtie-build Solanum_tuberosum_hairpin.fa Solanum_tuberosum_hairpin_index
 
-bowtie-build hairpin_tcc.fa hairpin_tcc_index
-bowtie-build mature_tcc.fa mature_tcc_index
+bowtie-build hairpin_stu.fa hairpin_stu_index
+bowtie-build mature_stu.fa mature_stu_index
 
 # build bowtie index of the genome 
+
 bowtie-build ~/rna_seq_analysis/ref_dataset/read_mapping/Solanum_tuberosum.SolTub_3.0.dna.toplevel.fa ~/rna_seq_analysis/ref_dataset/read_mapping/Solanum_tuberosum.SolTub_3.0.dna.toplevel_index
+
+#fastq to fasta
+
+seqkit fq2fa ~/rna_seq_analysis/cacao_mirna_analysis/Fastqc/SRR26332880_trimmed.fastq.gz -o SRR26332880_trimmed.fasta
+
+sed 's/^\([^ ]*\).*$/\1/' SRR26332881_trimmed.fasta > fixed_SRR26332881_trimmed.fasta
+
+# process reads and map them to the genome.
+
+mapper.pl ~/rna_seq_analysis/cacao_mirna_analysis/sra_data/SRR26332880.fasta -c -j -l 18 -m -p ~/rna_seq_analysis/cacao_mirna_analysis/alignment/Theobroma_cacao_index -s ~/rna_seq_analysis/cacao_mirna_analysis/alignment/SRR26332880_processed_reads.fa -t ~/rna_seq_analysis/cacao_mirna_analysis/alignment/SRR26332880_reads_vs_genome.arf -v 
+
+
+mapper.pl ~/rna_seq_analysis/cacao_mirna_analysis/sra_data/SRR26332880.fasta -c -j -k -l 18 -m -p fixed_genome.fa -s SRR26332880reads_collapsed.fa -t SRR26332880reads_collapsed_vs_genome.arf -v
+
+mapper.pl fixed_SRR26332883_trimmed.fasta -c -j -l 18 -m -p fixed_genome_index -s SRR26332883_processed_reads.fa -t SRR26332883_reads_vs_genome.arf -v
+
+# The -c option designates that the input file is a FASTA file (for other input formats, see the README.md file). 
+# The -j options removes entries with non-canonical letters (letters other than a, c, g, t, u, n, A, C, G, T, U, or N). 
+# The -k option clips adapters. The -l option discards reads shorter than 18 nts. The -m option collapses the reads. 
+# The -p option maps the processed reads against the previously indexed genome (cel_cluster). 
+# The -s option designates the name of the output file of processed reads and the -t option designates the name of the output file of the genome mappings. 
+# Last, -v gives verbose output to the screen.
+
+sed -i 's/ /_/g' ~/rna_seq_analysis/cacao_mirna_analysis/alignment/Theobroma_cacao.fa
+
+# reduce header description
+
+#sed -E 's/^(>[^:]+).*/\1/' ~/rna_seq_analysis/cacao_mirna_analysis/alignment/Theobroma_cacao.fa > fixed_genome.fa
+sed -E 's/^(>[^:]+)[^ ]*/\1/' ~/rna_seq_analysis/cacao_mirna_analysis/alignment/Theobroma_cacao.fa | sed 's/_dna//' > fixed_genome.fa
+grep ">" fixed_genome.fa
+
+#Explanation
+s/^(>[^:]+).*/\1/
+^> → Matches the FASTA header (lines starting with >)
+[^:]+ → Captures everything before the first colon (:)
+.* → Matches everything after the colon
+\1 → Keeps only the first part before :
+s/_dna// → Removes _dna from the remaining header.
+
+src/miRDeep2.pl /home/aswani/rna_seq_analysis/cacao_mirna_analysis/alignment/SRR26332880_processed_reads.fa fixed_genome.fa corrected.arf /home/aswani/rna_seq_analysis/cacao_mirna_analysis/alignment/mirna/pimren_Mature_miRNA_seq.fas fixed_mirna_mirbase.fa none none > mirna_results.txt
+
+# fast quantitation of reads mapping to known miRBase precursors.
+# (This step is not required for identification of known and novel miRNAs in the deep sequencing data when using miRDeep2.pl.)
+# The miRNA_expressed.csv gives the read counts of the reference miRNAs in the data in tabular format. The results can also be browsed by opening expression_16_19.html with an internet browser.
+
+quantifier.pl -p precursors_ref_this_species.fa -m mature_ref_this_species.fa -r reads_collapsed.fa -t cel -y 16_19
+
+# identification of known and novel miRNAs in the deep sequencing data:
+
+miRDeep2.pl reads_collapsed.fa cel_cluster.fa reads_collapsed_vs_genome.arf mature_ref_this_species.fa mature_ref_other_species.fa precursors_ref_this_species.fa -t C.elegans 2> report.log
+
+src/miRDeep2.pl /home/aswani/rna_seq_analysis/cacao_mirna_analysis/alignment/SRR26332880_processed_reads.fa fixed_genome.fa corrected.arf /home/aswani/rna_seq_analysis/cacao_mirna_analysis/alignment/mirna/pimren_Mature_miRNA_seq.fas fixed_mirna_mirbase.fa none none > mirna_results.txt
+mapper.pl SRR13986902aligned_reads.fasta -c -j -m -p fixed_cassava_genome_index -s SRR13986902_processed_reads.fa -t SRR13986902_reads_vs_genome.arf
+
+
+cut -f6 SRR26332880_reads_vs_genome.arf | sort | uniq > arf_ids.txt
+
+grep ">" fixed_genome.fa | cut -d ' ' -f1 | sed 's/>//' > genome_ids.txt
+
+# remove header length
+
+awk '{if(substr($0,1,1)==">") print $1; else print $0}' hairpin.fa > hairpin_fixed.fa
+
+miRDeep2.pl reads_collapsed.fa cel_cluster.fa reads_collapsed_vs_genome.arf mature_ref_this_species.fa mature_ref_other_species.fa precursors_ref_this_species.fa -t C.elegans 2> report.log
+miRDeep2.pl ~/rna_seq_analysis/cacao_mirna_analysis/alignment/SRR26332880_processed_reads.fa ~/rna_seq_analysis/miRDeep2/fixed_genome.fa ~/rna_seq_analysis/miRDeep2/corrected.arf pimren_Mature_miRNA_seq.fas fixed_mature.fa miRNA_stem-loop_seq.fas
+
+
 
 
